514e7cfc707547d671c9849afdcb8f6fcb658c59
GustavoCostaAM <112218656+GustavoCostaAM@users.noreply.github.com>
2024-09-11 21:53:28 -0300

    Delete germinareHome/week2 directory

--- germinareHome/week2/praticas/pratica5.py
+++
@@ -1,8 +0,0 @@
-# EXERCICIO 5 -  tabuada:
-# Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
-
-# feito sem copiar e/ou auxilios
-numero = int(input('escreva um numero e veja sua tabuada: '))
-
-for x in range(1, 11):
-    print(f'{numero} x {x} = {numero*x}')
