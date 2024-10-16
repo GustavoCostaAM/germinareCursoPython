e68fc12ee798d9aaa0d8b963d8ff3150c9781107
GustavoCostaAM <112218656+GustavoCostaAM@users.noreply.github.com>
2024-10-16 13:08:23 -0300

    Delete germinareHome directory

--- germinareHome/week4/praticas/pratica3.py
+++
@@ -1,9 +0,0 @@
-numero1 = int(input('Insira o primeiro valor: '))
-numero2 = int(input('Insira o segundo valor: '))
-
-if(numero1 > numero2):
-    print('O primeiro numero é maior')
-elif(numero2 > numero1):
-    print('O segundo numero é maior')
-else:
-    print('Os dois numeros sao iguais')
--- germinareHome/week6/praticas/pratica4.py
+++
@@ -1,3 +0,0 @@
-nomeCompleto = list(input('Insira seu nome completo: ').strip().split())
-
-print(f'\nO primeiro nome é {nomeCompleto[0]}, e o ultimo é {nomeCompleto[-1]}')
