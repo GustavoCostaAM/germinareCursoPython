3fe3d5fd55ba2e0524b5eb3ba2ce0d05fc0d1925
GustavoCostaAM <112218656+GustavoCostaAM@users.noreply.github.com>
2024-11-06 22:27:26 -0300

    Delete germinareHome directory

--- germinareHome/week7/praticas/pratica2.py
+++
@@ -1,5 +0,0 @@
-from random import shuffle
-
-listaNomes = input('Digite o nume de cada pessoa, com um espaco de distancia para separar: ').strip().split()
-shuffle(listaNomes)
-print(f'A ordem sorteada é: {listaNomes}')
--- germinareHome/week7/praticas/pratica3.py
+++
@@ -1,5 +0,0 @@
-def calcularArea(comprimento, largura):
-    print(f'\nA area total é de {comprimento*largura} metros quadrados')
-
-
-calcularArea(float(input('Insira o comprimento em metros2: ')), float(input('Insira a largura em metros2: ')))
